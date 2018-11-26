6f7d4d51c3d17b5cab6644471dd7b12cd49ec37a
user <user@users-MacBook-Pro-2.local>
2018-11-26 16:46:41 +0000

    added employee and manager sections

---
+++ src/main/java/Staff/Employee.java
@@ -0,0 +1,36 @@
+package Staff;
+
+
+public abstract class Employee {
+
+    private String name;
+    private int niNumber;
+    private double salary;
+
+    public Employee(String name, int niNumber, double salary){
+        this.name = name;
+        this.niNumber = niNumber;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getNiNumber() {
+        return this.niNumber;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void raiseSalary(double amount){
+        this.salary += amount;
+    }
+
+    public double payBonus(){
+       return (0.01 * this.salary);
+    }
+
+}
---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,43 @@
+import Management.Manager;
+import Staff.Employee;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ManagerTest {
+
+    Employee employee;
+
+    @Before
+    public void setup(){
+        employee = new Manager("Bob", 1234, 30000, "Admissions");
+    }
+
+    @Test
+    public void canGetName(){
+       assertEquals("Bob", employee.getName());
+    }
+
+    @Test
+    public void canGetNINumber(){
+        assertEquals(1234, employee.getNiNumber());
+    }
+
+    @Test
+    public void canGetSalary(){
+        assertEquals(30000, employee.getSalary(), 0.00001);
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        employee.raiseSalary(1000);
+        assertEquals(31000, employee.getSalary(), 0.00001);
+    }
+
+    @Test
+    public void canPayBonus(){
+        double bonus = employee.payBonus();
+        assertEquals(30300, (bonus + employee.getSalary()), 0.00001);
+    }
+}
